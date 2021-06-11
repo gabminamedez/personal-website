63d4b30ef8ee9682511f326b2d048297a63fa05f
gabminamedez <gabminamedez@gmail.com>
2021-06-11 16:01:26 +0800

    Doing Portfolio

--- pages/portfolio.js
+++ pages/portfolio.js
@@ -1,11 +1,17 @@
+import { Container, Row, Col } from 'reactstrap';
+
 import Head from '../components/Head';
+
+import portfolioStyles from '../styles/portfolio.module.css';
 
 const Portfolio = () => {
     return (
         <div>
             <Head title={'Gab Minamedez | Portfolio'} />
 
-            <h1>Portfolio</h1>
+            <Container className={portfolioStyles.portfolio}>
+                <h1>My Work</h1>
+            </Container>
         </div>
     );
 }
