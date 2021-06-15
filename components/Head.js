75c8ff7a583c14826c5cd644939098c39498bf62
gabminamedez <gabminamedez@gmail.com>
2021-06-15 20:06:55 +0800

    Updated HTML head deets for SEO

--- components/Head.js
+++ components/Head.js
@@ -5,7 +5,7 @@ const Meta = ({title}) => {
         <Head>
             <meta name='viewport' content='width=device-width, initial-scale=1' />
             <meta name='keywords' content='Gabriel Minamedez, Computer Science, Web Developer, De La Salle University' />
-            <meta name='description' content='Gabriel Minamedez is a Computer Science in De La Salle University based in Manila, Philippines. He is an aspiring web developer and machine learning engineer. He also writer for SLAM Philippines. © 2021 Gab Minamedez.' />
+            <meta name='description' content='Gabriel Minamedez is a junior Computer Science student in De La Salle University based in Manila, Philippines. He is an aspiring web developer and machine learning engineer. He also writes for SLAM Philippines. © 2021 Gab Minamedez.' />
             <meta charSet='utf-8' />
             <link rel='icon' href='/img/favicon.ico' />
             <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css' />
