3d25204918157091c199d9839baa0ded5d54c735
Gabriel Minamedez <gabminamedez@gmail.com>
2022-10-20 14:31:36 +0800

    Changed Medium to Substack in Portfolio; Some other bugfixes

--- frontend/src/pages/Portfolio.js
+++ frontend/src/pages/Portfolio.js
@@ -1,25 +1,16 @@
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import { Container, Row, Col } from 'reactstrap';
 
 import Meta from '../components/Meta'; 
 import { develop } from '../assets/data/pfDevelop';
 import { design } from '../assets/data/pfDesign';
 import { photography } from '../assets/data/pfPhotography';
+import sansserif from '../assets/images/portfolio/sansserif.png';
 
 import portfolioStyles from '../assets/styles/Portfolio.module.css';
 
 const Portfolio = () => {  
     const [selected, setSelected] = useState('develop');
-    const [articles, setArticles] = useState([]);
-
-    useEffect(() => {
-        fetch(`https://api.rss2json.com/v1/api.json?rss_url=https://medium.com/feed/@gabminamedez`)
-        .then((res) => res.json()).then((response) => {
-            setArticles(response.items);
-        })
-        .catch(err => console.log(err));
-        
-    }, [articles]);
 
     return (
         <div>
@@ -64,15 +55,16 @@ const Portfolio = () => {
                                         </div>
                                     </Col>)
                     }) }
-                    {   selected === 'writing' && articles && articles.map((item) => {
-                            return (<Col lg={6}>
-                                        <div className={portfolioStyles.project}>
-                                            <a href={ item.link } target='_blank' rel='noreferrer'><h2>{ item.title }</h2></a>
-                                            <img src={ item.thumbnail } alt='img' />
-                                        </div>
-                                    </Col>)
-                    }) }
-                    {   selected === 'photography' && articles && photography.map((item) => {
+                    {   selected === 'writing' &&
+                            <Col lg={6}>
+                                <div className={portfolioStyles.project}>
+                                    <a href='https://sansserif.substack.com/' target='_blank' rel='noreferrer'><h2>Subscribe to Sans Serif!</h2></a>
+                                    <p className={portfolioStyles.metadata}>A trail of word vomits to navigate this beautiful, twisted, modern existence that I attempt to make sense of.</p>
+                                    <img src={ sansserif } alt='img' />
+                                </div>
+                            </Col>
+                    }
+                    {   selected === 'photography' && photography && photography.map((item) => {
                             return (<Col lg={6}>
                                         <div className={portfolioStyles.project}>
                                             <a href={ item.url } target='_blank' rel='noreferrer'><h2>{ item.title }</h2></a>
