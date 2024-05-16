19984efd7c3962247d5a93472bb6602439210fe5
gabminamedez <gabminamedez@gmail.com>
2024-05-16 20:27:00 +0800

    Update Brand and MenuButton styles

--- frontend/src/components/Brand.tsx
+++ frontend/src/components/Brand.tsx
@@ -6,7 +6,7 @@ const Brand = () => {
   return (
     <Link to="/">
       <div className={brandStyles.brand}>
-        <h1 className="font-black absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 pointer-events-none text-[50px]">
+        <h1 className="font-black absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 pointer-events-none text-[90px]">
           G
         </h1>
       </div>
